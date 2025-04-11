3778ad246c3737d10fadb9fc4910843b01658238
Astra66666 <s06.p.me@gmail.com>
2025-04-11 13:02:55 +0530

    Add kernel.mk with OSS kernel build config

--- kernel.mk
+++ kernel.mk
@@ -1,4 +1,7 @@
-# Kernel build flags
+# Kernel build settings
 TARGET_KERNEL_SOURCE := kernel/xiaomi/raphael
 TARGET_KERNEL_CONFIG := raphael_defconfig
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := clang-r450784d
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
